95056c1de584098dbb3ee3405e15d0f8d2d95082
ferreiraklet <90654646+ferreiraklet@users.noreply.github.com>
2023-02-06 20:21:51 -0300

    Update wolfy.py

--- wolfy.py
+++ wolfy.py
@@ -88,14 +88,14 @@ class Wolfy:
         f_build.write(f_crypto_data)
         f_build.close()
 
-        print("[] - Run the following command and type enter when done.")
+        print("[] - Run the following command to compile your executable:")
         print("Linux:")
         print(f'sudo wine "$(find ~/.wine/drive_c/ -type f -name pyinstaller.exe)" --onefile --noconsole --distpath {os.getcwd()}/ -i {os.getcwd()}/icons/{icon}.ico {os.getcwd()}/build/{exename}.py --key={key}')
         print("Windows:")
         print(f'pyinstaller --onefile --noconsole --distpath {os.getcwd()}\\ -i {os.getcwd()}\\icons\\{icon}.ico {os.getcwd()}\\build\\{exename}.py --key={key}')
         #subprocess.call(f'mv "{exename}.exe" "{exename}-not-signed.exe";osslsigncode sign -certs certificate/cert.pem -key certificate/cert.key -n "{exename}" -i https://microsoft.com/ -in "{exename}-not-signed.exe" -out "{exename}.exe";rm "{exename}-not-signed.exe"', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         #print(f'mv "{exename}.exe" "{exename}-not-signed.exe";osslsigncode sign -certs certificate/cert.pem -key certificate/cert.key -n "{exename}" -i https://microsoft.com/ -in "{exename}-not-signed.exe" -out "{exename}.exe";rm "{exename}-not-signed.exe"')
-        #print(f"[INFO] - DONE! Your .exe is {exename}.exe")
+        print("[INFO] - DONE!")
 
 if __name__ == '__main__':
     c = Wolfy()
